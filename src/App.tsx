eb08b3aa8296c7f41a11f4d93ec4f45edfa0e46a
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-18 12:01:05 +0200

    before lunch

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,27 @@ function App() {
 		language: string;
 		yearMonth: string;
 		numberInStock: number;
+		hasError: boolean;
 	}
+
+	const userIsAdmin = true;
 
 	useEffect(() => {
 		(async () => {
-			const rawBooks:any[] = (await axios.get(url)).data;
+			const rawBooks: any[] = (await axios.get(url)).data;
 			const _books: IBook[] = [];
-			rawBooks.forEach((rawBook:any) => {
+			rawBooks.forEach((rawBook: any) => {
 				const book: IBook = {
 					id: rawBook.id,
 					title: rawBook.title,
 					description: rawBook.description,
-					language: rawBook.language,
+					language: rawBook.language ? rawBook.language : 'english',
 					yearMonth: rawBook.yearMonth,
 					numberInStock: rawBook.numberInStock,
-				}
+					hasError: ['english', 'french'].includes(rawBook.language)
+						? false
+						: true,
+				};
 				_books.push(book);
 			});
 			setBooks(_books);
@@ -44,34 +50,38 @@ function App() {
 			<div className="bookArea">
 				{books.map((book, i) => {
 					return (
-						<fieldset className="book" key={i}>
-							<legend>ID: {book.id}</legend>
+						<>
+							{!book.hasError && (
+								<fieldset className="book" key={i}>
+									<legend>ID: {book.id}</legend>
 
-							<div className="row">
-								<label>Title</label>
-								<div>{book.title}</div>
-							</div>
+									<div className="row">
+										<label>Title</label>
+										<div>{book.title}</div>
+									</div>
 
-							<div className="row">
-								<label>Description</label>
-								<div>{book.description}</div>
-							</div>
+									<div className="row">
+										<label>Description</label>
+										<div>{book.description}</div>
+									</div>
 
-							<div className="row">
-								<label>Language</label>
-								<div>{book.language}</div>
-							</div>
+									<div className="row">
+										<label>Language</label>
+										<div>{book.language}</div>
+									</div>
 
-							<div className="row">
-								<label>Year/Month</label>
-								<div>{book.yearMonth}</div>
-							</div>
+									<div className="row">
+										<label>Year/Month</label>
+										<div>{book.yearMonth}</div>
+									</div>
 
-							<div className="row">
-								<label>In Stock</label>
-								<div>{book.numberInStock}</div>
-							</div>
-						</fieldset>
+									<div className="row">
+										<label>In Stock</label>
+										<div>{book.numberInStock}</div>
+									</div>
+								</fieldset>
+							)}
+						</>
 					);
 				})}
 			</div>

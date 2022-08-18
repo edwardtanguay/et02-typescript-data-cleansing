b08f519f54bb9bbccc2e0a80338256df9f601f93
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-18 11:38:16 +0200

    basic axios api works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.scss'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
+
+const url =
+	'https://edwardtanguay.netlify.app/share/techBooksUnstructured.json';
 
 function App() {
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setBooks((await axios.get(url)).data);
+		})();
+	}, []);
+
 	return (
 		<div className="App">
-		<h1>TypeScript Site Example</h1>
-		<p>testing</p>
+			<h1>TypeScript Site Example</h1>
+			<h2>There are {books.length} books.</h2>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
